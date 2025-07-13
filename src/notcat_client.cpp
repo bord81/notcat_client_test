92d353a2180d5dcb5c3c34164b7243871b99a63f
Borys Zakaliuk <bslabut@gmail.com>
2025-07-13 17:25:55 +0200

    add more test logs to rust and cpp examples

--- src/notcat_client.cpp
+++ src/notcat_client.cpp
@@ -1,5 +1,11 @@
 #include "notcat_client.h"
+#include <chrono>
 #include <iostream>
+#include <random>
+#include <string>
+#include <thread>
+
+using namespace std::chrono_literals;
 
 int main() {
     auto* client = notcat_connect("/dev/socket/notcat_socket");
@@ -10,6 +16,19 @@ int main() {
     if (notcat_log(client, "Hello C++!\n") != 0) {
         std::cerr << "send failed\n";
     }
+    std::mt19937_64 eng{std::random_device{}()};
+    for (int i = 0; i < 1000; ++i) {
+        std::uniform_int_distribution<int> dist(5, 100);
+        std::string message = "Message number from C++ " + std::to_string(i) + "\n";
+        if (notcat_log(client, message.c_str()) != 0) {
+            std::cerr << "send failed\n";
+        }
+        std::this_thread::sleep_for(dist(eng) * 1ms);
+    }
+    if (notcat_log(client, "C++ finished test\n") != 0) {
+        std::cerr << "send failed\n";
+    }
+    std::this_thread::sleep_for(876000h);
     notcat_close(client);
     return 0;
 }
