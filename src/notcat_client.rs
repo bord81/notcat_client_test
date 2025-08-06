5265eba47d4f6d3e8f843ce76afdada0bbe72701
Borys Zakaliuk <bslabut@gmail.com>
2025-08-06 22:21:43 +0200

    refactor log api

--- src/notcat_client.rs
+++ src/notcat_client.rs
@@ -4,39 +4,32 @@ use rand::Rng;
 use std::{thread, time::Duration};
 
 fn main() -> std::io::Result<()> {
+    let log_tag = "Rust Client";
     log_init(notcat_lib::ANDROID_LOGCAT_SINK | notcat_lib::LOCAL_FILE_SINK)?;
-    log(LogPriority::Info, b"Main", b"Hello Rust!\n");
+    logi!(log_tag, "Hello Rust!\n");
     for i in 0..10 {
         thread::spawn(move || {
             for j in 0..100 {
                 let delay = rand::thread_rng().gen_range(5..100);
                 thread::sleep(Duration::from_millis(delay));
-                log(
-                    LogPriority::Debug,
-                    b"Main",
-                    format!(
-                        "Message number from Rust thread {} (with delay) {}\n",
-                        i,
-                        (i * 10) + j
-                    )
-                    .as_bytes(),
+                logd!(
+                    log_tag,
+                    "Message number from Rust thread {} (with delay) {}\n",
+                    i,
+                    (i * 10) + j
                 );
             }
             for k in 101..200 {
-                log(
-                    LogPriority::Debug,
-                    b"Main",
-                    format!(
-                        "Message number from Rust thread {} (no delay) {}\n",
-                        i,
-                        (i * 10) + k
-                    )
-                    .as_bytes(),
+                logd!(
+                    log_tag,
+                    "Message number from Rust thread {} (no delay) {}\n",
+                    i,
+                    (i * 10) + k
                 );
             }
         });
     }
-    log(LogPriority::Info, b"Main", b"Rust client finished test\n");
+    logi!(log_tag, "Rust client finished test\n");
     thread::sleep(Duration::from_secs(u32::MAX.into()));
     close()?;
     Ok(())
